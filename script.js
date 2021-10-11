416d23afc5ede2767101b06208a2bb29ed350cbd
eulaur69 <77895139+eulaur69@users.noreply.github.com>
2021-10-11 12:47:24 +0300

    added canvas context

--- script.js
+++ script.js
@@ -20,4 +20,6 @@ function capteaza() {
     var c = document.getElementById("canvas");
     c.width = video.width;
     c.height = video.height;
+    var ctx = c.getContext("2d");
+    ctx.drawImage(video, 0, 0, 640, 480);
 }
